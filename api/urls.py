d4b4de343adb80b8a176000cd66d0c6c31c4b2ed
bahrur <bahrur1256@gmail.com>
2024-12-06 14:29:58 +0700

    add model and setting

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from api import views as api_views
+
+urlpatterns = [
+    path('user/token/',api_views.MyTokenObtainPairView.as_view()),
+    path('user/token/refresh/',TokenRefreshView.as_view()),
+    path('user/register/',api_views.RegisterView.as_view()),
+]
